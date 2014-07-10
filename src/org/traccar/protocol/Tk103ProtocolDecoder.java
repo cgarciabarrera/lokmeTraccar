b79425eee42ec1eeeeae0f452ca3a9848325f046
Carlos García de la Barrera <carlos.garcia@lextrend.net>
2014-07-10 20:02:08 +0200

    Version 2 en macha

--- src/org/traccar/protocol/Tk103ProtocolDecoder.java
+++ src/org/traccar/protocol/Tk103ProtocolDecoder.java
@@ -29,123 +29,124 @@ import org.traccar.model.Position;
 
 public class Tk103ProtocolDecoder extends BaseProtocolDecoder {
 
-    public Tk103ProtocolDecoder(ServerManager serverManager) {
-        super(serverManager);
-    }
+	public Tk103ProtocolDecoder(ServerManager serverManager) {
+		super(serverManager);
+	}
 
-    static private Pattern pattern = Pattern.compile(
-            "(\\d{12})" +                // Device ID
-            ".{4}" +                     // Command
-            "\\d*" +                     // IMEI (?)
-            "(\\d{2})(\\d{2})(\\d{2})" + // Date (YYMMDD)
-            "([AV])" +                   // Validity
-            "(\\d{2})(\\d{2}\\.\\d{4})" + // Latitude (DDMM.MMMM)
-            "([NS])" +
-            "(\\d{3})(\\d{2}\\.\\d{4})" + // Longitude (DDDMM.MMMM)
-            "([EW])" +
-            "(\\d+\\.\\d)" +             // Speed
-            "(\\d{2})(\\d{2})(\\d{2})" + // Time (HHMMSS)
-            "(\\d+\\.?\\d+)" +           // Course
-            "([0-9a-fA-F]{8})" +         // State
-            "L([0-9a-fA-F]+)");          // Milage
+	static private Pattern pattern = Pattern.compile("(\\d{12})" + // Device ID
+			".{4}" + // Command
+			"\\d*" + // IMEI (?)
+			"(\\d{2})(\\d{2})(\\d{2})" + // Date (YYMMDD)
+			"([AV])" + // Validity
+			"(\\d{2})(\\d{2}\\.\\d{4})" + // Latitude (DDMM.MMMM)
+			"([NS])" + "(\\d{3})(\\d{2}\\.\\d{4})" + // Longitude (DDDMM.MMMM)
+			"([EW])" + "(\\d+\\.\\d)" + // Speed
+			"(\\d{2})(\\d{2})(\\d{2})" + // Time (HHMMSS)
+			"(\\d+\\.?\\d+)" + // Course
+			"([0-9a-fA-F]{8})" + // State
+			"L([0-9a-fA-F]+)"); // Milage
 
-    @Override
-    protected Object decode(
-            ChannelHandlerContext ctx, Channel channel, Object msg)
-            throws Exception {
+	@Override
+	protected Object decode(ChannelHandlerContext ctx, Channel channel,
+			Object msg) throws Exception {
 
-        String sentence = (String) msg;
-        
-        // Find message start
-        int beginIndex = sentence.indexOf('(');
-        if (beginIndex != -1) {
-            sentence = sentence.substring(beginIndex + 1);
-        }
+		String sentence = (String) msg;
 
-        // TODO: Send answer?
-        //(090411121854AP05)
+		// Find message start
+		int beginIndex = sentence.indexOf('(');
+		if (beginIndex != -1) {
+			sentence = sentence.substring(beginIndex + 1);
+		}
 
-        // Parse message
-        Matcher parser = pattern.matcher(sentence);
-        if (!parser.matches()) {
-            return null;
-        }
+		// TODO: Send answer?
+		// (090411121854AP05)
 
-        // Create new position
-        Position position = new Position();
-        ExtendedInfoFormatter extendedInfo = new ExtendedInfoFormatter("tk103");
-        Integer index = 1;
+		// Parse message
+		Matcher parser = pattern.matcher(sentence);
+		if (!parser.matches()) {
+			return null;
+		}
 
-        // Get device by IMEI
-        String imei = parser.group(index++);
-        try {
-            position.setDeviceId(getDataManager().getDeviceByImei(imei).getId());
-            
-        } catch(Exception error) {
-            // Compatibility mode (remove in future)
-            try {
-                position.setDeviceId(getDataManager().getDeviceByImei("000" + imei).getId());
-            } catch(Exception error2) {
-                Log.warning("Unknown device - " + imei);
-                Log.warning("Unknown device - " + imei + " creating new...");
-                getDataManager().addDevice(imei);
-                Log.warning("Created device - " + imei + " OK");
-                //deviceId = getDataManager().getDeviceByImei(imei).getId();
-                //deviceIMEI= imei;
-                //return null;
-            }
-        }
+		// Create new position
+		Position position = new Position();
+		ExtendedInfoFormatter extendedInfo = new ExtendedInfoFormatter("tk103");
+		Integer index = 1;
 
-        // Date
-        Calendar time = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-        time.clear();
-        time.set(Calendar.YEAR, 2000 + Integer.valueOf(parser.group(index++)));
-        time.set(Calendar.MONTH, Integer.valueOf(parser.group(index++)) - 1);
-        time.set(Calendar.DAY_OF_MONTH, Integer.valueOf(parser.group(index++)));
+		// Get device by IMEI
+		String imei = parser.group(index++);
+		try {
+			position.setDeviceId(getDataManager().getDeviceByImei(imei).getId());
 
-        // Validity
-        position.setValid(parser.group(index++).compareTo("A") == 0 ? true : false);
+		} catch (Exception error) {
+			// Compatibility mode (remove in future)
+			try {
+				position.setDeviceId(getDataManager().getDeviceByImei(
+						"000" + imei).getId());
+			} catch (Exception error2) {
+				Log.warning("Unknown device - " + imei);
+				Log.warning("Unknown device - " + imei + " creating new...");
+				getDataManager().addDevice(imei);
+				Log.warning("Created device - " + imei + " OK");
+				// deviceId = getDataManager().getDeviceByImei(imei).getId();
+				// deviceIMEI= imei;
+				// return null;
+			}
+		}
 
-        // Latitude
-        Double latitude = Double.valueOf(parser.group(index++));
-        latitude += Double.valueOf(parser.group(index++)) / 60;
-        if (parser.group(index++).compareTo("S") == 0) latitude = -latitude;
-        position.setLatitude(latitude);
-        
-        position.setDeviceIMEI(imei);
+		// Date
+		Calendar time = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+		time.clear();
+		time.set(Calendar.YEAR, 2000 + Integer.valueOf(parser.group(index++)));
+		time.set(Calendar.MONTH, Integer.valueOf(parser.group(index++)) - 1);
+		time.set(Calendar.DAY_OF_MONTH, Integer.valueOf(parser.group(index++)));
 
-        // Longitude
-        Double longitude = Double.valueOf(parser.group(index++));
-        longitude += Double.valueOf(parser.group(index++)) / 60;
-        if (parser.group(index++).compareTo("W") == 0) longitude = -longitude;
-        position.setLongitude(longitude);
+		// Validity
+		position.setValid(parser.group(index++).compareTo("A") == 0 ? true
+				: false);
 
-        // Altitude
-        position.setDeviceIMEI(imei);;
-        
-        // Altitude
-        position.setAltitude(0.0);
+		// Latitude
+		Double latitude = Double.valueOf(parser.group(index++));
+		latitude += Double.valueOf(parser.group(index++)) / 60;
+		if (parser.group(index++).compareTo("S") == 0)
+			latitude = -latitude;
+		position.setLatitude(latitude);
 
-        // Speed
-        position.setSpeed(Double.valueOf(parser.group(index++)));
+		position.setDeviceIMEI(imei);
 
-        // Time
-        time.set(Calendar.HOUR, Integer.valueOf(parser.group(index++)));
-        time.set(Calendar.MINUTE, Integer.valueOf(parser.group(index++)));
-        time.set(Calendar.SECOND, Integer.valueOf(parser.group(index++)));
-        position.setTime(time.getTime());
+		// Longitude
+		Double longitude = Double.valueOf(parser.group(index++));
+		longitude += Double.valueOf(parser.group(index++)) / 60;
+		if (parser.group(index++).compareTo("W") == 0)
+			longitude = -longitude;
+		position.setLongitude(longitude);
 
-        // Course
-        position.setCourse(Double.valueOf(parser.group(index++)));
-        
-        // State
-        extendedInfo.set("state", parser.group(index++));
+		// Altitude
+		position.setDeviceIMEI(imei);
+		;
 
-        // Milage
-        extendedInfo.set("milage", Integer.parseInt(parser.group(index++), 16));
+		// Altitude
+		position.setAltitude(0.0);
 
-        position.setExtendedInfo(extendedInfo.toString());
-        return position;
-    }
+		// Speed
+		position.setSpeed(Double.valueOf(parser.group(index++)));
+
+		// Time
+		time.set(Calendar.HOUR, Integer.valueOf(parser.group(index++)));
+		time.set(Calendar.MINUTE, Integer.valueOf(parser.group(index++)));
+		time.set(Calendar.SECOND, Integer.valueOf(parser.group(index++)));
+		position.setTime(time.getTime());
+
+		// Course
+		position.setCourse(Double.valueOf(parser.group(index++)));
+
+		// State
+		extendedInfo.set("state", parser.group(index++));
+
+		// Milage
+		extendedInfo.set("milage", Integer.parseInt(parser.group(index++), 16));
+
+		position.setExtendedInfo(extendedInfo.toString());
+		return position;
+	}
 
 }
